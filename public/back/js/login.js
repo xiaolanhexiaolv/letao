9fe8be640ccb983288308aac46cba767a03da8b6
xiaolanhexiaolv <359286540@qq.com>
2018-11-21 16:04:29 +0800

    登录功能提交校验完成

--- public/back/js/login.js
+++ public/back/js/login.js
@@ -32,6 +32,9 @@ $(function() {
             min: 2,
             max: 6,
             message: "用户名长度必须是2-6位"
+          },
+          callback:{
+            message:'用户名不存在'
           }
         }
       },
@@ -45,7 +48,12 @@ $(function() {
             min: 6,
             max: 12,
             message: "密码长度必须是6-12位"
+          },
+          // 专门用于ajax回调提示说明
+          callback:{
+            message:'密码错误'
           }
+
         }
       }
     },
@@ -66,11 +74,11 @@ $('#form').on("success.form.bv",function(e){
                 location.href ="index.html";
             }
             if(info.error === 1000){
-                alert(info.message);
+                $('#form').data('bootstrapValidator').updateStatus('username','INVALID','callback')
             }
 
             if(info.error === 1001){
-                alert(info.message);
+               $('#form').data("bootstrapValidator").updateStatus('password', 'INVALID', 'callback');
             }
 
         }
