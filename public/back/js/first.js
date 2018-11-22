6296e886324ce5e97c906a24cfd2ead99ca14fa3
xiaolanhexiaolv <359286540@qq.com>
2018-11-22 20:36:37 +0800

    一级分类添加完成

--- public/back/js/first.js
+++ public/back/js/first.js
@@ -20,7 +20,7 @@ $(function(){
                 // 指定版本号
                 bootstrapMajorVersion:3,
                 currentPage: info.page,
-                totalPage: Math.ceil(info.total / info.size),
+                totalPages: Math.ceil(info.total / info.size),
                 onPageClicked: function(a,b,c,page){
                     // console.log(page);
                     currentPage = page;
@@ -35,7 +35,60 @@ $(function(){
     });
 
     }
-    
+    // 点击添加按钮，显示模态框
 
+    $('#addBtn').click(function(){
+        $('#addModal').modal('show');
+    });
 
+    // 3、表单校验功能
+    $('#form').bootstrapValidator({
+        //配置小图标
+
+        feedbackIcons: {
+            valid: 'glyphicon glyphicon-ok',   // 校验成功
+            invalid: 'glyphicon glyphicon-remove',   // 校验失败
+            validating: 'glyphicon glyphicon-refresh'  // 校验中
+        },
+
+        // 配置字段
+
+        fields: {
+            categoryName: {
+                
+            validators: {
+                //不能为空
+                notEmpty: {
+                message: '请输入一级分类名称'
+                }
+
+            }
+
+          }
+
+         } 
+
+    });
+
+    // 4 注册表单校验成功实践，阻止默认的表单提交，通过ajax提交
+
+    $('form').on('success.form.bv',function(e){
+        e.preventDefault();
+        $.ajax({
+            type: 'post',
+            url: '/category/addTopCategory',
+            data: $('#form').serialize(),
+            dataType:'json',
+            success: function(info){
+                console.log(info);
+                if(info.success){
+                    $('#addModal').modal('hide');
+                    currentPage = 1;
+                    render();
+                    $('#form').data('bootstrapValidator').resetForm(true);
+                }
+            }
+        })
+
+    })
 })
